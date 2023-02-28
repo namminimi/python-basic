e4947782abed582668667226e00a1fca06f39f5d
namminimi <tbvhdaos@naver.com>
2023-02-28 12:06:16 +0900

    day01

--- day01/ex01.py
+++ day01/ex01.py
@@ -2,3 +2,16 @@
 str = "hello python"
 print(str)
 print(type(str))
+
+num = 10
+print(type(num))
+
+#문자 "", '', ''' ''', """ """
+str2 = """안녕하세요
+안녕하세요
+안녕하세요"""
+
+str3 = "안녕하세요 \"안녕하세요 \" 안녕하세요"   """ 이스케이프 문자/코드 """
+
+print(str2)
+print(str3)
---
+++ ex02.py
@@ -0,0 +1,30 @@
+num1 = 10
+num2 = 3
+
+
+#연산자
+#+:덧셈, -:뺄셈, *:곱셈, /:나눗셈, **:제곱근, %:나머지, //:몫(정수)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 + num2)
+print(num1 - num2)
+
+#문자열 곱하기 *
+print("=" * 20)
+
+#문자열 더하기 +
+head = "Python"
+tail = "is funs"
+print(head+tail)
+print(head*2)
+#문자열 길이구하기 1en(변수)
+a = "python"
+print(len(a))
+
+#문자열 인덱싱
+print(a[5])
+#문자열 슬라이싱 문자열[시작번호: 끝번호] - 끝번호 제외
+print(a[0:2])
+print(a[:3])
+print(a[2:])
